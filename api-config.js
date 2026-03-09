c4e280eed0a7440bbee9a8288dff7727552db1e8
vanamala shivaji ganesh <vanamalashivaji19@gmail.com>
2026-03-09 15:38:13 +0530

    api fix

--- api-config.js
+++ api-config.js
@@ -9,7 +9,7 @@ const MODE = "Prod";
 const API_BASE_URL =
   MODE === "local"
     ? "http://localhost:3000"
-    : "https://health-tracker-backend-9928u0d7f-vanamala19s-projects.vercel.app";
+    : "https://health-tracker-backend-one.vercel.app";
 
 // Export for use in all modules (if using ES6 modules)
 // export { API_BASE_URL };
